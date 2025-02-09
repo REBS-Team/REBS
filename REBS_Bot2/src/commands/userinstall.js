4a9c08def652ed62b2f4ead03d1f685f705eafae
Ringible <143556044+Ringible@users.noreply.github.com>
2025-02-09 01:20:49 +0000

    Nineth Commit

---
+++ REBS_Bot2/src/commands/userinstall.js
@@ -0,0 +1,7 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('userinstalltesting')
+    .setDescription('User Install Testing')
+}
