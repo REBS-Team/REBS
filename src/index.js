932e8af2c85ce2657aa569bd18fcf94dece4b2de
Ringible <143556044+Ringible@users.noreply.github.com>
2025-02-21 18:30:11 +0000

    Update code and fix module issues

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+// filepath: /workspaces/REBS/src/index.js
+require('dotenv').config();
+
+console.log(`Bot token: ${process.env.token}`); // Add this line to verify the token
+
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require('discord.js');
 const fs = require('fs');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); 
 
 client.commands = new Collection();
-
-require('dotenv').config();
 
 const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
 const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
